78b9ea714cfbafbefce00136e0fa1ce92920981a
fvillagrans <fvillag@gmail.com>
2016-06-20 09:39:44 -0500

    Add files via upload

---
+++ package.js
@@ -0,0 +1,20 @@
+{
+  "name": "streaming",
+  "version": "1.0.0",
+  "description": "app para streaming de video",
+  "main": "streaming.js",
+  "dependencies": {
+    "npm": "^2.7.4",
+    "socket.io": "^1.3.5"
+  },
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "start": "node server.js"
+  },
+  "repository": {
+    "type": "git",
+    "url": "xxx"
+  },
+  "author": "fvillagrans",
+  "license": "ISC"
+}
---
+++ streaming.js
@@ -0,0 +1,14 @@
+// se crea un servidor http automaticamente
+var io = require('socket.io')(8080);
+
+// recibe un objeto socket cada que un cliente se conectta
+io.on('connection', function (objSocket) {
+	
+	console.log('nuevo usuario conectado');
+	
+	// se recibe un evento newFrame conteniendo una imagen
+	objSocket.on('newFrame', function ( img ) {
+		io.emit("setFrame", img);
+	});
+
+});
